7579cf2dd00832e1efd99e900027b11048f6fce0
tyllo <gmtyllo@gmail.com>
2016-07-04 01:30:59 +1000

    update serializeQueryParam and deserializeQueryParam for beter parse array query params

--- app/pods/index/route.js
+++ app/pods/index/route.js
@@ -10,6 +10,22 @@ export default Ember.Route.extend({
     controller.set('meta', model.get('meta'));
   },
 
+  serializeQueryParam(value, urlKey, defaultValueType) {
+    if (defaultValueType === 'array') {
+      return JSON.stringify(value).replace(/"/g, '');
+    }
+
+    return this._super(...arguments);
+  },
+
+  deserializeQueryParam(value, urlKey, defaultValueType) {
+    if (defaultValueType === 'array') {
+      return value.replace(/(\[|\])/g, '').split(',');
+    }
+
+    return this._super(...arguments);
+  },
+
   actions: {
     queryParamsDidChange(/*changed, totalPresent, removed*/) {
       this.refresh();
